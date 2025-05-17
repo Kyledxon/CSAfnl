7fb51653a51ddc4bab5927cec8835604e73e03d7
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-05-16 19:23:06 -0700

    working on customer class

---
+++ PapasPizza/src/customer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class customer {
+		private int xPos = 500;
+		private int yPos = 500;
+		
+		public customer(int posX, int posY) {
+			posX = xPos;
+			posY = yPos;
+		}
+		
+		
+		private static String order() {
+			ArrayList<String> orders = new ArrayList<String>();
+			
+			return "";
+		}
+		
+		
+		
+		
+}
