f977f9612fe30bed8f6f0977f06d4c87c733b188
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-05-28 11:58:41 -0700

    trying to get customers into order screen

--- PapasPizza/src/OrderScreen.java
+++ PapasPizza/src/OrderScreen.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.io.File;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -13,6 +14,34 @@ public class OrderScreen extends GameScreen {
     public OrderScreen() {
     	
     }
+    
+//    public class CustomerLoader {
+//
+//        public static ArrayList<customer> loadCustomersFromFolder(String folderPath) {
+//            ArrayList<customer> customers = new ArrayList<>();
+//
+//            File folder = new File(folderPath);
+//            File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".png") || 
+//                                                            name.toLowerCase().endsWith(".jpg") ||
+//                                                            name.toLowerCase().endsWith(".jpeg"));
+//
+//            if (files != null) {
+//                for (File file : files) {
+//                    String fileName = file.getName();
+//                    String name = fileName.substring(0, fileName.lastIndexOf(".")); // e.g., "tony.png" -> "tony"
+//                    ImageIcon image = new ImageIcon(file.getPath());
+//
+//                    // Assign random position or use default ones
+//                    int x = 900;
+//                    int y = 500;
+//
+//                   // customer customer = new customer(name, x, y, image);
+//                   //.add(customer);
+//                }
+//            }
+//
+//            return customers;
+//        }
 
     @Override
     public void onShow() {
