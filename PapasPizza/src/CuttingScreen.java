45fb3cc5fdc7a822056b377e770849fe7fefb381
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-05-30 11:40:01 -0700

    cutting screen

--- PapasPizza/src/CuttingScreen.java
+++ PapasPizza/src/CuttingScreen.java
@@ -27,12 +27,20 @@ public class CuttingScreen extends GameScreen implements MouseListener{
     	ArrayList<Sprite2> placedToppings = Game.toppingsScreen.getPlacedToppings();
     	
     	sprites.clear();
+    	ImageIcon bg = new ImageIcon("images/cutting-pixel.png");
+    	Image bgImage = bg.getImage();
         ImageIcon pizza = new ImageIcon("images/pizza crust1.png");
     	Image pizzaImage = pizza.getImage();
 
     	//hi
+    	sprites.add(new Sprite(0, 0, bgImage, 1.0));
 
-    	sprites.add(new Sprite(200, 100, pizzaImage, .75));
+    	sprites.add(new Sprite(90, 130, pizzaImage, .6));
+    	
+    	for(Sprite2 topping : placedToppings) {
+    		sprites.add(new Sprite((int)(90 + (topping.getX() - 200)/1.37), (int)(130 + (topping.getY() - 100)/1.37), topping.getImage(), topping.getScale()/1.37));
+    	}
+    	
     	
     	for(Sprite2 topping : placedToppings) {
     		sprites.add(new Sprite(topping.getX(), topping.getY(), topping.getImage(), topping.getScale()));
