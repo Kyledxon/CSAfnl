88ede4af5f68651a32a1bdea3c14f2516fd8d74b
Heth Bhatt <140643847+hethb@users.noreply.github.com>
2025-05-30 11:04:16 -0700

    cutting logic started

--- PapasPizza/src/CuttingScreen.java
+++ PapasPizza/src/CuttingScreen.java
@@ -1,21 +1,61 @@
 import java.awt.Color;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class CuttingScreen extends GameScreen {
+public class CuttingScreen extends GameScreen implements MouseListener{
 
     public CuttingScreen() {
-        
+    	 int numCuts; 
+    	    int deltaX;
+    	    int deltaY;
+    	    int startX;
+    	    int endX;
+    	    int startY;
+    	    int endY;
     }
 
+   
     @Override
     public void onShow() {
         sprites.clear();
         ImageIcon pizza = new ImageIcon("images/pizza crust1.png");
     	Image pizzaImage = pizza.getImage();
     	sprites.add(new Sprite(0, 0, pizzaImage, .5));
+    	//hi
     	
     }
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		 
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
